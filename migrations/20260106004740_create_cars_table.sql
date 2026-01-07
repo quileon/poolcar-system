ae0e6eca5f6b021352de3893f112de733f8ba1e6
quileon <121283381+quileon@users.noreply.github.com>
2026-01-07 19:44:25 +0700

    fix: make cars reference tracker one-to-one

--- migrations/20260106004740_create_cars_table.sql
+++ migrations/20260106004740_create_cars_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE cars (
     police_number VARCHAR(255) UNIQUE NOT NULL,
     active BOOLEAN NOT NULL DEFAULT TRUE,
     car_type_id INTEGER NOT NULL REFERENCES car_types(car_type_id),
-    tracker_id INTEGER NULL REFERENCES trackers(tracker_id),
+    tracker_id INTEGER NULL UNIQUE REFERENCES trackers(tracker_id),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     deleted_at TIMESTAMP NULL
