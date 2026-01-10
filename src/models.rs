7a3bfacffa2cb16dc209bc1bda61bf8abee2c808
quileon <121283381+quileon@users.noreply.github.com>
2026-01-10 16:20:30 +0700

    feat: create tracker payload models

--- src/models.rs
+++ src/models.rs
@@ -65,3 +65,94 @@ pub struct PaginationParams {
     pub page: Option<u32>,
     pub limit: Option<u32>,
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerConnection {
+    pub interval: u32,
+    pub retries: u32,
+    pub sequence_id: u32,
+    pub iteration_id: u32,
+    pub strength: u32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerLocation {
+    pub latitude: Option<f64>,
+    pub longitude: Option<f64>,
+    pub age: Option<u32>,
+    pub valid: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerAltitude {
+    pub meters: Option<f32>,
+    pub feet: Option<f32>,
+    pub age: Option<u32>,
+    pub valid: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerSpeed {
+    pub kmph: Option<f32>,
+    pub mph: Option<f32>,
+    pub mps: Option<f32>,
+    pub knots: Option<f32>,
+    pub age: Option<u32>,
+    pub valid: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerCourse {
+    pub degrees: Option<f32>,
+    pub age: Option<u32>,
+    pub valid: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerDateTime {
+    pub iso8601: Option<String>,
+    pub year: Option<u32>,
+    pub month: Option<u8>,
+    pub day: Option<u8>,
+    pub hour: Option<u8>,
+    pub minute: Option<u8>,
+    pub second: Option<u8>,
+    pub millisecond: Option<u32>,
+    pub valid: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerSatellites {
+    pub count: Option<u8>,
+    pub age: Option<u32>,
+    pub valid: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerHdop {
+    pub value: Option<f32>,
+    pub age: Option<u32>,
+    pub valid: bool,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerStats {
+    pub chars_processed: Option<u32>,
+    pub sentences_with_fix: Option<u32>,
+    pub failed_checksum: Option<u32>,
+    pub passed_checksum: Option<u32>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TrackerPayload {
+    pub uptime: u32,
+    pub connection: TrackerConnection,
+    pub location: TrackerLocation,
+    pub altitude: TrackerAltitude,
+    pub speed: TrackerSpeed,
+    pub course: TrackerCourse,
+    pub datetime: TrackerDateTime,
+    pub satellites: TrackerSatellites,
+    pub hdop: TrackerHdop,
+    pub stats: TrackerStats,
+}
