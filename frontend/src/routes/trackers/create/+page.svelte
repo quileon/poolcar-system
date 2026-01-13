16eca08194b2f02f0775338244534beb903873f5
quileon <121283381+quileon@users.noreply.github.com>
2026-01-13 11:03:53 +0700

    feat: create logic for creating car

--- frontend/src/routes/trackers/create/+page.svelte
+++ frontend/src/routes/trackers/create/+page.svelte
@@ -1,11 +1,47 @@
 <script lang="ts">
+	import * as Alert from "$lib/components/ui/alert/index";
+	import AlertCircleIcon from "@lucide/svelte/icons/alert-circle";
 	import Button from "$lib/components/ui/button/button.svelte";
 	import * as Field from "$lib/components/ui/field/index";
 	import Input from "$lib/components/ui/input/input.svelte";
+	import { createMutation, useQueryClient } from "@tanstack/svelte-query";
+	import { goto } from "$app/navigation";
+	import { resolve } from "$app/paths";
+
+	let trackerName = "";
+
+	const queryClient = useQueryClient();
+
+	const mutation = createMutation(() => ({
+		mutationFn: async (trackerName: string) => {
+			const response = await fetch("http://localhost:3000/trackers", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ name: trackerName })
+			});
+
+			if (!response.ok) {
+				throw new Error("Failed to create tracker");
+			}
+
+			return response.json();
+		},
+		onSuccess: async () => {
+			await queryClient.invalidateQueries({ queryKey: ["trackers"] });
+			goto(resolve("/trackers"));
+		}
+	}));
+
+	function handleSubmit(e: Event) {
+		e.preventDefault();
+		mutation.mutate(trackerName);
+	}
 </script>
 
 <div class="mx-auto w-full max-w-md">
-	<form>
+	<form onsubmit={handleSubmit}>
 		<Field.Group>
 			<Field.Set>
 				<Field.Legend>Create Tracker</Field.Legend>
@@ -14,14 +50,33 @@
 				<Field.Group>
 					<Field.Field>
 						<Field.Label for="name">Tracker Name</Field.Label>
-						<Input id="name" name="name" type="text" placeholder="Enter tracker name" required />
+						<Input
+							id="name"
+							bind:value={trackerName}
+							type="text"
+							placeholder="Enter tracker name"
+							required
+						/>
 					</Field.Field>
 				</Field.Group>
 			</Field.Set>
 			<Field.Field orientation="horizontal">
-				<Button type="submit">Submit</Button>
-				<Button variant="outline" type="button">Cancel</Button>
+				<Button type="submit" disabled={mutation.isPending}>Submit</Button>
+				<Button variant="outline" type="button" disabled={mutation.isPending} href="/trackers"
+					>Cancel</Button
+				>
 			</Field.Field>
 		</Field.Group>
 	</form>
+	<div class="mt-8">
+		{#if mutation.isError}
+			<Alert.Root variant="destructive">
+				<AlertCircleIcon />
+				<Alert.Title>Error</Alert.Title>
+				<Alert.Description>
+					<p>{mutation.error.message}</p>
+				</Alert.Description>
+			</Alert.Root>
+		{/if}
+	</div>
 </div>
