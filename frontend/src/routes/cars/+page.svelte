53cdd0abe3efc107fc5ff6b8008d309064a9358b
quileon <121283381+quileon@users.noreply.github.com>
2026-01-13 08:45:49 +0700

    feat: create filter and and modify button on cars

--- frontend/src/routes/cars/+page.svelte
+++ frontend/src/routes/cars/+page.svelte
@@ -5,6 +5,21 @@
 	import AlertCircleIcon from "@lucide/svelte/icons/alert-circle";
 	import * as Alert from "$lib/components/ui/alert/index";
 	import * as Table from "$lib/components/ui/table/index";
+	import * as Select from "$lib/components/ui/select/index";
+	import * as ButtonGroup from "$lib/components/ui/button-group/index";
+	import Button from "$lib/components/ui/button/button.svelte";
+	import ArrowRightIcon from "@lucide/svelte/icons/arrow-right";
+	import PencilIcon from "@lucide/svelte/icons/pencil";
+
+	const filters = [
+		{ label: "Active", value: "active" },
+		{ label: "Deleted", value: "deleted" },
+		{ label: "All", value: "all" }
+	];
+	let filterValue = $state("");
+	const filterTrigger = $derived(
+		filters.find((filter) => filter.value === filterValue)?.label ?? "Filter by"
+	);
 
 	const carsQuery = createQuery<CarWithTracker[]>(() => ({
 		queryKey: ["cars"],
@@ -19,7 +34,15 @@
 <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Cars Management</h1>
 
 {#if carsQuery.isPending}
-	<Skeleton class="rounded-xl" />
+	<div class="flex flex-col gap-4">
+		<Skeleton class="h-12" />
+		<div class="flex flex-col space-y-1">
+			<Skeleton class="h-6 w-full rounded-xl" />
+			<Skeleton class="h-6 w-full rounded-xl" />
+			<Skeleton class="h-6 w-full rounded-xl" />
+			<Skeleton class="h-6 w-full rounded-xl" />
+		</div>
+	</div>
 {/if}
 
 {#if carsQuery.isError}
@@ -33,6 +56,31 @@
 {/if}
 
 {#if carsQuery.isSuccess}
+	<div class="flex justify-end">
+		<ButtonGroup.Root>
+			<ButtonGroup.Root>
+				<Select.Root type="single" name="filter" bind:value={filterValue}>
+					<Select.Trigger class="w-45">
+						{filterTrigger}
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.Label>Filters</Select.Label>
+							{#each filters as filter (filter.value)}
+								<Select.Item value={filter.value} label={filter.label} />
+							{/each}
+						</Select.Group>
+					</Select.Content>
+				</Select.Root>
+				<Button variant="outline" aria-label="Send" size="icon">
+					<ArrowRightIcon />
+				</Button>
+			</ButtonGroup.Root>
+			<ButtonGroup.Root>
+				<Button href="/cars/create" size="default" variant="outline">Create Car</Button>
+			</ButtonGroup.Root>
+		</ButtonGroup.Root>
+	</div>
 	<Table.Root>
 		<Table.Caption>A list of cars.</Table.Caption>
 		<Table.Header>
@@ -43,6 +91,7 @@
 				<Table.Head>Active</Table.Head>
 				<Table.Head>Car Type</Table.Head>
 				<Table.Head>Tracker</Table.Head>
+				<Table.Head>Actions</Table.Head>
 			</Table.Row>
 		</Table.Header>
 		<Table.Body>
@@ -54,6 +103,11 @@
 					<Table.Cell>{car.active ? "Yes" : "No"}</Table.Cell>
 					<Table.Cell>{car.car_type_name}</Table.Cell>
 					<Table.Cell>{car.tracker_name}</Table.Cell>
+					<Table.Cell>
+						<Button href={`/cars/${car.car_id}`} size="icon" variant="outline">
+							<PencilIcon />
+						</Button>
+					</Table.Cell>
 				</Table.Row>
 			{/each}
 		</Table.Body>
