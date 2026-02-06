cf3741a47acdb4dfe44e574525027898cace9729
quileon <121283381+quileon@users.noreply.github.com>
2026-02-06 07:26:39 +0700

    feat: add authentication error

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -1,41 +1,52 @@
 use axum::{http::StatusCode, response::IntoResponse};
 use thiserror::Error;
 
+/// Error types for Axum.
+///
+/// Instead of handling the error directly and responding the client with the error message.
+/// This code automatically handles the error message, outputing into stderr while also responding with appropriate messages to the client.
 #[derive(Error, Debug)]
 pub enum AppError {
-    #[error("Database error")]
+    #[error("Internal server error")]
     Database(#[from] sqlx::Error),
 
-    #[error("Password hashing failed")]
+    #[error("Internal server error")]
     HashError(#[from] argon2::password_hash::Error),
 
-    #[error("Invalid password or usernameemail")]
-    InvalidLogin,
-
     #[error("Page not found")]
-    NotFound,
+    PageNotFound,
 
     #[error("Missing field")]
     MissingField,
+
+    #[error("Wrong credentials")]
+    WrongCredentials,
+
+    #[error("Invalid token")]
+    InvalidToken,
+
+    #[error("Internal server error")]
+    EncodingError,
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> axum::response::Response {
-        let (status, message) = match &self {
+        let message = self.to_string();
+
+        let status = match &self {
             AppError::Database(error_message) => {
-                eprintln!("DATABASE ERROR: {:?}", error_message);
-                (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
+                eprintln!("Database error: {:?}", error_message);
+                StatusCode::INTERNAL_SERVER_ERROR
             }
             AppError::HashError(error_message) => {
-                eprintln!("HASHING ERROR: {:?}", error_message);
-                (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
+                eprintln!("Hashing error {:?}", error_message);
+                StatusCode::INTERNAL_SERVER_ERROR
             }
-            AppError::InvalidLogin => {
-                eprintln!("Wrong credentials");
-                (StatusCode::UNAUTHORIZED, "Wrong credentials")
-            }
-            AppError::NotFound => (StatusCode::NOT_FOUND, "Not found"),
-            AppError::MissingField => (StatusCode::BAD_REQUEST, "Missing field"),
+            AppError::PageNotFound => StatusCode::NOT_FOUND,
+            AppError::MissingField => StatusCode::BAD_REQUEST,
+            AppError::WrongCredentials => StatusCode::UNAUTHORIZED,
+            AppError::InvalidToken => StatusCode::UNAUTHORIZED,
+            AppError::EncodingError => StatusCode::INTERNAL_SERVER_ERROR,
         };
 
         (status, message).into_response()
