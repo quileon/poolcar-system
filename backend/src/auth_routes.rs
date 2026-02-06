5bd5f33b8722f9fb90d7b5c2fc100d0a837b1736
quileon <121283381+quileon@users.noreply.github.com>
2026-02-06 07:31:53 +0700

    feat: update login page to return an encoded JWT token

--- backend/src/auth_routes.rs
+++ backend/src/auth_routes.rs
@@ -1,37 +1,50 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum::{extract::State, response::IntoResponse, Json};
+use chrono::{Duration, Utc};
 use std::sync::Arc;
 
 use crate::{
-    models::{LoginRequest, UserAuth},
+    auth_utils::{encode_jwt, verify_password},
+    error::AppError,
+    models::{Claims, LoginRequest, LoginResponse, UserAuth},
     AppState,
 };
 
 pub async fn login_handler(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<LoginRequest>,
-) -> Result<impl IntoResponse, (StatusCode, String)> {
+) -> Result<impl IntoResponse, AppError> {
     let user = sqlx::query_as!(
         UserAuth,
         r#"
             SELECT
                 users.username,
                 users.password,
-                users.user_role_id
+                users.user_role_id,
+                user_roles.name AS user_role_name
             FROM users
+            LEFT JOIN user_roles ON users.user_role_id = user_roles.user_role_id
             WHERE users.username = $1
             AND users.deleted_at IS NULL
         "#,
         payload.username
     )
-    .fetch_one(&state.db)
-    .await
-    .map_err(|e| {
-        eprintln!("Database error: {:?}", e);
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Database error: {}", e),
-        )
-    })?;
+    .fetch_optional(&state.db)
+    .await?
+    .ok_or(AppError::WrongCredentials)?;
 
-    Ok(axum::Json(user))
+    if !verify_password(&payload.password, &user.password) {
+        return Err(AppError::WrongCredentials);
+    }
+
+    let expiration = (Utc::now() + Duration::hours(1)).timestamp() as usize;
+
+    let claims = Claims {
+        username: payload.username,
+        role_name: user.user_role_name,
+        exp: expiration,
+    };
+
+    let token = encode_jwt(claims, &state.config.jwt_secret)?;
+
+    Ok(Json(LoginResponse { token }))
 }
