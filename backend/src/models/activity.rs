f6a3177fa679b7ad5034c066d643111137f729d0
quileon <121283381+quileon@users.noreply.github.com>
2026-02-12 09:15:56 +0700

    feat: create export and get single activity routes

--- backend/src/models/activity.rs
+++ backend/src/models/activity.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 use ts_rs::TS;
@@ -28,4 +29,15 @@ pub struct ActivityWithCount {
 pub struct GetActivitiesResponse {
     pub activities: Vec<ActivityWithCount>,
     pub activity_count: usize,
+}
+
+#[derive(Debug, FromRow, Deserialize, Serialize, PartialEq, TS)]
+#[ts(export)]
+pub struct ActivityExport {
+    pub activity_id: i32,
+    pub name: String,
+    pub activity_count: Option<i64>,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+    pub deleted_at: Option<NaiveDateTime>,
 }
--- backend/src/routes/activity_routes.rs
+++ backend/src/routes/activity_routes.rs
@@ -1,13 +1,16 @@
 use crate::{
     error::AppError,
-    models::activity::{Activity, ActivityBody, ActivityWithCount, GetActivitiesResponse},
+    models::activity::{
+        Activity, ActivityBody, ActivityExport, ActivityWithCount, GetActivitiesResponse,
+    },
     types::PaginationParams,
     AppState,
 };
 use axum::{
     extract::{Path, Query, State},
+    http::StatusCode,
     response::IntoResponse,
-    routing::{get, put},
+    routing::get,
     Json, Router,
 };
 use std::sync::Arc;
@@ -49,6 +52,98 @@ pub async fn get_activities(
     };
 
     Ok(Json(response))
+}
+
+pub async fn get_activity(
+    State(state): State<Arc<AppState>>,
+    Path(activity_id): Path<i32>,
+) -> Result<impl IntoResponse, AppError> {
+    let activity = sqlx::query_as!(
+        ActivityWithCount,
+        r#"
+            SELECT
+                activities.activity_id,
+                activities.name,
+                COUNT(histories.history_id) as activity_count
+            FROM activities
+            LEFT JOIN histories ON histories.activity_id = activities.activity_id
+            WHERE activities.activity_id = $1
+            GROUP BY activities.activity_id, activities.name
+            ORDER BY activities.activity_id ASC
+        "#,
+        activity_id
+    )
+    .fetch_one(&state.db)
+    .await?;
+
+    Ok(Json(activity))
+}
+
+pub async fn export_activities(
+    State(state): State<Arc<AppState>>,
+) -> Result<impl IntoResponse, AppError> {
+    let activities = sqlx::query_as!(
+        ActivityExport,
+        r#"
+            SELECT
+                activities.activity_id,
+                activities.name,
+                COUNT(histories.history_id) as activity_count,
+                activities.created_at,
+                activities.updated_at,
+                activities.deleted_at
+            FROM activities
+            LEFT JOIN histories ON histories.activity_id = activities.activity_id
+            WHERE activities.deleted_at IS NULL
+            GROUP BY activities.activity_id, activities.name
+            ORDER BY activities.activity_id ASC
+        "#
+    )
+    .fetch_all(&state.db)
+    .await?;
+
+    let mut csv_buffer = Vec::new();
+    {
+        let mut writer = csv::Writer::from_writer(&mut csv_buffer);
+        writer.write_record(&[
+            "Activity ID",
+            "Name",
+            "Activity Count",
+            "Created At",
+            "Updated At",
+            "Deleted At",
+        ])?;
+
+        for activity in activities {
+            writer.write_record(&[
+                activity.activity_id.to_string(),
+                activity.name,
+                activity
+                    .activity_count
+                    .map(|count| count.to_string())
+                    .unwrap_or_default(),
+                activity.created_at.to_string(),
+                activity.updated_at.to_string(),
+                activity
+                    .deleted_at
+                    .map(|count| count.to_string())
+                    .unwrap_or_default(),
+            ])?;
+        }
+        writer.flush()?;
+    }
+
+    Ok((
+        StatusCode::OK,
+        [
+            ("Content-Type", "text/csv"),
+            (
+                "Content-Disposition",
+                "attachment; filename=\"activities.csv\"",
+            ),
+        ],
+        csv_buffer,
+    ))
 }
 
 pub async fn create_activity(
@@ -115,8 +210,11 @@ pub async fn delete_activity(
 pub fn routes() -> Router<Arc<AppState>> {
     Router::new()
         .route("/", get(get_activities).post(create_activity))
+        .route("/export", get(export_activities))
         .route(
             "/{activity_id}",
-            put(update_activity).delete(delete_activity),
+            get(get_activity)
+                .put(update_activity)
+                .delete(delete_activity),
         )
 }
