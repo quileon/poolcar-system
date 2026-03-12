2aa6409e0bd4d22f8916930dabf2a786bad3b802
quileon <121283381+quileon@users.noreply.github.com>
2026-03-12 10:57:38 +0700

    fix: change frontend building into using bun adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "svelte-adapter-bun";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,7 +15,7 @@ const config = {
 			pages: "build",
 			assets: "build",
 			fallback: "index.html",
-			precompress: false,
+			precompress: true,
 			strict: true
 		})
 	}
