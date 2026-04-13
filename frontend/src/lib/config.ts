4087994eb9e8c11bd61b39a7865f73c01b1c41c6
quileon <121283381+quileon@users.noreply.github.com>
2026-04-13 15:35:12 +0700

    fix: make env dynamically loaded

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,14 +1,14 @@
-import { PUBLIC_API_BASE_URL, PUBLIC_WS_BASE_URL } from "$env/static/public";
+import { env } from "$env/dynamic/public";
 
-if (!PUBLIC_API_BASE_URL) {
+if (!env.PUBLIC_API_BASE_URL) {
 	throw new Error("PUBLIC_API_BASE_URL environment variable is not defined");
 }
 
-if (!PUBLIC_WS_BASE_URL) {
+if (!env.PUBLIC_WS_BASE_URL) {
 	throw new Error("PUBLIC_WS_BASE_URL environment variable is not defined");
 }
 
 export const config = {
-	apiBaseUrl: PUBLIC_API_BASE_URL,
-	wsBaseUrl: PUBLIC_WS_BASE_URL
+	apiBaseUrl: env.PUBLIC_API_BASE_URL,
+	wsBaseUrl: env.PUBLIC_WS_BASE_URL
 } as const;
